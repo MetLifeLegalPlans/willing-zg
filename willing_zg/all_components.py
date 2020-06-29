cf4204586f6fc17889428d92494b80f627807f4e
JCog <jordancogan4@gmail.com>
2020-06-29 18:18:27 -0400

    WORKFLOW: remove custom_server import for all_components

--- willing_zg/all_components.py
+++ willing_zg/all_components.py
@@ -1,6 +1,5 @@
 from zygoat.components import Component
 
-from .custom_server import custom_server
 from .email import email
 from .simple_jwt import simple_jwt
 from .tokens import token_util
@@ -10,5 +9,5 @@ class AllComponents(Component):
     pass
 
 
-# excluding 'deployment' for now - causes the github workflow to fail to due calling 'eb init'
-all_components = AllComponents(sub_components=[custom_server, email, simple_jwt, token_util])
+# excluding 'deployment' and 'custom_server' for now - causes the github workflow to fail to due calling 'eb init'
+all_components = AllComponents(sub_components=[email, simple_jwt, token_util])
