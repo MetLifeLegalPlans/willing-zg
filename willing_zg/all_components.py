e5e3a8155a0af16ec6900a67d40df8f3db1c4309
JCog <jordancogan4@gmail.com>
2020-06-29 18:13:40 -0400

    WORKFLOW: remove deployment import for all_components

--- willing_zg/all_components.py
+++ willing_zg/all_components.py
@@ -1,7 +1,6 @@
 from zygoat.components import Component
 
 from .custom_server import custom_server
-from .deployment import deployment
 from .email import email
 from .simple_jwt import simple_jwt
 from .tokens import token_util
